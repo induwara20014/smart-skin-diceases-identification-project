f8c9d16cc8beb7f630028a641cbb7a812f68214a
induwara20014 <lakshaninduwara067@gmail.com>
2026-05-06 11:23:45 +0530

    add

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+import forms from "@tailwindcss/forms";
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {}
+  },
+  plugins: [forms]
+};
+
